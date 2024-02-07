483b2cb851d1f5df2f321c06b0a9e8617a2d3427
beas-t <128628778+beas-t@users.noreply.github.com>
2024-02-07 14:57:06 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,13 @@
+public class Greatest{
+    public static void main(String[] args) {
+
+
+       int a=10;
+       int b=7;
+       int c=5;
+       if(a>b && a>c){
+           System.out.println("The Greatest Number is "+a);
+       }
+    }
+
+}
---
+++ Two_Dimensional_Array.java
@@ -0,0 +1,15 @@
+public class Two_Dimensional_Array {
+    public static void main(String[] args) {
+        int i, j;
+        int a[][] = {{1, 2, 3}, {4, 5, 6,}, {7, 8, 9}};
+        for (i = 0; i < a.length; i++) {
+            for (j = 0; j < a.length; j++) {
+                System.out.println(a[i][j]);
+            }
+        }
+    }
+}
+
+
+
+
