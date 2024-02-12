65873a691e02ba7d1d3079e92451028d8606e630
beas-t <128628778+beas-t@users.noreply.github.com>
2024-02-12 13:54:36 +0530

    Update Anagram.java

--- Day 2/Anagram.java
+++ Day 2/Anagram.java
@@ -6,20 +6,14 @@ public class Anagram {
 
             str1 = str1.toLowerCase();
             str2 = str2.toLowerCase();
-
-            // check if length is same
+            
             if(str1.length() == str2.length()) {
-
-                // convert strings to char array
                 char[] charArray1 = str1.toCharArray();
                 char[] charArray2 = str2.toCharArray();
 
-                // sort the char array
+               
                 Arrays.sort(charArray1);
                 Arrays.sort(charArray2);
-
-                // if sorted char arrays are same
-                // then the string is anagram
                 boolean result = Arrays.equals(charArray1, charArray2);
 
                 if(result) {
