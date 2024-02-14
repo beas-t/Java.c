5dde97f01429b6741d99e9e9bef394a3f7e68675
beas-t <128628778+beas-t@users.noreply.github.com>
2024-02-14 13:04:28 +0530

    Delete Lowercase.java

--- Lowercase.java
+++
@@ -1,7 +0,0 @@
-public class Lowercase {
-    public static void main(String[] args) {
-        String s = "My name is mike";
-        String c = s.toLowerCase();
-        System.out.println(c);
-    }
-}
