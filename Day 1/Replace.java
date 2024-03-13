02389f10248e720171fdf17709181bcaa98f79d6
beas-t <128628778+beas-t@users.noreply.github.com>
2024-03-13 14:54:12 +0530

    Delete Day 1 directory

--- Day 1/Replace.java
+++
@@ -1,9 +0,0 @@
-public class Replace{
-    public static void main(String[] args) {
-        String s="My name is mike";
-        String c=s.replace('i','j');
-        System.out.println(c);
-
-    }
-
-}
